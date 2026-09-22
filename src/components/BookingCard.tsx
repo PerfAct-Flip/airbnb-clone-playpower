15d509a704f65a4d61aa3f958b16d8bc90cb0587
Soham Sinha <sohamsinha208@gmail.com>
2026-09-22 19:44:14 +0530

    fix : centred the content of discount card

--- src/components/BookingCard.tsx
+++ src/components/BookingCard.tsx
@@ -42,7 +42,7 @@ export function BookingCard({
 
   return (
     <div ref={cardRef} className="sticky top-30 pt-6">
-      <div className="flex items-center gap-3 border border-(--color-border) rounded-2xl px-4 pb-4 mb-4">
+      <div className="flex items-center gap-3 border border-(--color-border) rounded-2xl px-4 p-4 pb-4 mb-4">
         <Sparkles size={20} className="text-[#2b6b4f] shrink-0" />
         <div className="flex-1 text-sm">
           <span className="font-medium">Get 10% off your next stay.</span>{" "}
